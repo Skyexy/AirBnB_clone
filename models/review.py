b351dbec8398beec9448364121edd2b4045c6e4c
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-08-07 22:02:38 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-""" review module """
+"""Defines the Review class."""
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """ Review module """
-    text = ""
+    """Represent a review.
+    Attributes:
+        place_id (str): The Place id.
+        user_id (str): The User id.
+        text (str): The text of the review.
+    """
+
     place_id = ""
     user_id = ""
-
-    def __init__(self, *arg, **kwargs):
-        """ defualt """
-        super(Review, self).__init__(**kwargs)
+    text = ""
