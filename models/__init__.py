578c04b748f33654652641493a79b0cd51e1931e
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-08-08 08:54:47 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,7 @@
-""" default Module """
+#!/usr/bin/python3
+"""__init__ magic method for models directory"""
 from models.engine.file_storage import FileStorage
+
 
 storage = FileStorage()
 storage.reload()
